4c3b8973d0391df37f4a383717303a6e3b2be83a
Evg <chilicklic@mail.ru>
2019-07-03 01:09:35 +0600

    21. Основное задание
    21. Основное задание: оживляем интерактивные элементы в личном проекте
    
    ____________________
    Базовые критерии

---
+++ js/modal.js
@@ -0,0 +1,60 @@
+var link = document.querySelector(".write-form-button");
+  
+  var popup = document.querySelector(".modal-write");
+  var close = popup.querySelector(".modal-close");
+  
+  var form = popup.querySelector("form");
+  var writeusr = popup.querySelector("[name=writeusr]");
+  var usremail = popup.querySelector("[name=usremail]");
+  var usrtext = popup.querySelector("[name=usrtext]");
+  
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("writeusr");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    
+    if (storage) {
+      writeusr.value = storage;
+      usremail.focus();
+      usrtext.focus();
+    } else {
+      writeusr.focus();
+    }
+  });
+  
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+  
+  form.addEventListener("submit", function (evt) {
+    if (!writeusr.value || !usremail.value || !usrtext.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("writeusr", writeusr.value);
+      }
+    }
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
